cf89960e35460943224a2199b403f5780d742abf
N-Il <47373830+n-Il@users.noreply.github.com>
2020-02-20 10:55:09 +0000

    functional, and readme updated

--- DailyBuilder.py
+++ DailyBuilder.py
@@ -1,15 +1,21 @@
+#Matt Harris
+#02/20/2020
+#Converting mbox to problems for fun/practice.
 import re
 import mailbox
+import os
 mbox_file = "ProblemsExport.mbox"
 mbox = mailbox.mbox(mbox_file)
+os.mkdir("output")
 for key in mbox.iterkeys():
     try:
         message = mbox[key]
     except mbox.errors.MessageParseError:
         continue  # skip the oof ones
     #if subject is a Daily Coding Problem
+    subject = str(message['Subject'])
     if subject.startswith("Daily Coding Problem"):
-        number = ubject.split("#")[1].split("[")[0][:-1]
+        number = subject.split("#")[1].split("[")[0][:-1]
         problem = str(message).split("Good morning!")[1].split("\nUpgrade to premium\n[https")[0] #this is lazy, should be using RE
         difficulty = subject.split("[")[1].split("]")[0]
         #print ("Subject:" + subject)
@@ -17,18 +23,14 @@ for key in mbox.iterkeys():
         #print("Difficulty:" + difficulty)
         #print("Content: " + problem)
         #print("********************************************")
-
         #problem = re.search('Good Morning!(.*) \n\n--',str(message)).group(0)
         #print("Content: "+problem)
         #example regex guesstimate
-
-        
 	#create directory called Problem### ex Problem1,Problem2,Problem3...
-        direct = "Problem" + number
-	if not os.path.exists(direct):
-		print("The directory "+ direct + " does already exists")
-	else:
-		os.mkdir(direct)    
-	#create README
-        #insert Good morning!+problem
-    
+        direct = "output/Problem" + number
+        os.mkdir(direct)
+        #create README
+        readMe = open(direct+"/README","w+")
+  	#insert Good morning!+problem
+        readMe.write(problem)
+        readMe.close()
--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-This project will take the .mbox file outputted from google takeout and turn it into daily problems folders for personal solution repo. I'm always behind, so I there are quite a few to catch up on..
-
-Ideas on formatting of mbox:
-starts with From:
-ends with NmP line?
-some emails are spam, so need to filter the subject for "Daily Coding Problem..."
-
-found some python starter code for going through each email.
-To get the subject(for the number), I can just go to the first instance of 'Subject: '
-Then, probabably skip until Good Morning!, assuming they don't change that intro, I can use that to grab the important part up to ----------------------------------------------------
-then the rest doesn't matter
+This project will take the .mbox file outputted from google takeout and turn it into daily problems folders for personal solution repo. I created this program because I don't always have the time do complete every daily problem. I like to queue up a few before a long plane trip, so this helps somewhat organize them.
